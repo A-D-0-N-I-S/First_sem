7982e2b6c8ad05aa8f526f2da3e7bfcf04eb0ad2
ADONIS <152091743+A-D-0-N-I-S@users.noreply.github.com>
2024-03-03 11:17:57 +0300

    Add files via upload

---
+++ 2 лабораторная/Laba2.3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cmath>
+
+using namespace std;
+// ВАРИАНТ 5
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double w, v, b, x, a;
+	b = 40;
+	x = 1.1;
+	a = 5e-6;
+
+	w = (a + b) * tan(x) / (x + 1);
+	v = 1 / 2 * b - sqrt(w - a * b);
+
+	cout << "Результат w равен " << w << endl << endl;
+	cout << "Результат v равен " << v << endl << endl;
+
+	system("pause");
+
+}
