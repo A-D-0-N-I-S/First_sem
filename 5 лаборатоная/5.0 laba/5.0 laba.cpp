bf73f90855ea3c53110ef869b486264cd594ec41
ADONIS <152091743+A-D-0-N-I-S@users.noreply.github.com>
2024-03-03 11:30:34 +0300

    Add files via upload

---
+++ 5 лаборатоная/5.0 laba/5.0 laba.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double w, z, a, t, y;
+
+	a = 1;
+	y = 0.4;
+	t = 5e-4;
+	
+	double j []{ 5,3,1,7,3 };
+
+	for (int i = 0; i < sizeof(j)/sizeof(j[0]); i++)
+	{
+		cout << "ЭТО " << i + 1 << " ИТЕРАЦИЯ\n";
+
+		z = sqrt(t * a + y) + 4 * exp(-2 * j[i]);
+		w = log(0.4 * y) / (7 * a - z);
+
+		cout << "z = " << z << endl;
+		cout << "w = " << w << endl<<endl;
+	}
+	
+
+
+
+
+}
---
+++ 5 лаборатоная/5.1/5.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double w, z, a, t, y, j;
+
+	a = 1;
+	y = 0.4;
+	t = 5e-4;
+	j = 1;
+	t = 1; // счётчик
+
+	while (j >= 1 && j <= 2)
+	{
+		cout << "ЭТО " << t << " ИТЕРАЦИЯ\n";
+		t++;
+
+		z = sqrt(t * a + y) + 4 * exp(-2 * j);
+		w = log(0.4 * y) / (7 * a - z);
+
+		cout << "j = " << j << endl;
+		cout << "z = " << z << endl;
+		cout << "w = " << w << endl << endl;
+
+		j = j + 0.2;
+		j = round(j * 10)/10;
+	}
+
+
+
+}
